30103b2ea261dafd579cc426b747492c069bde9d
Muzi Gumede <Muzigumede@outlook.com>
2020-04-25 23:19:10 +0200

    minor

--- playground/Play.hs
+++ playground/Play.hs
@@ -1,5 +1,4 @@
-{- Sum function defination -}
-
+{- recursive Sum function defination -}
 listSum [] = 0
 listSum (x:xs) = x + listSum (xs)
 
@@ -10,3 +9,4 @@ factorial x = x * factorial (x-1)
 {- sumation function defination -}
 addTwo :: Integer -> Integer -> Integer
 addTwo x y = x + y
+
